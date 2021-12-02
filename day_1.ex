e14a4eafac87e66a90343cb11febd459ff59f346
Juan Peri <eternoperegrino@gmail.com>
2021-12-02 09:08:26 +0100

    Day 1 Solution

---
+++ day_1.ex
@@ -0,0 +1,21 @@
+numbers =
+  File.stream!("day_1.txt")
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&String.to_integer/1)
+
+reducer = fn numbers ->
+  numbers
+  |> Enum.chunk_every(2, 1, :discard)
+  |> Enum.reduce(0, fn [a, b], acc -> if b > a, do: acc + 1, else: acc end)
+end
+
+result = reducer.(numbers)
+IO.inspect(result, label: "first challenge")
+
+result =
+  numbers
+  |> Enum.chunk_every(3, 1, :discard)
+  |> Enum.map(&Enum.sum/1)
+  |> reducer.()
+
+IO.inspect(result, label: "second challenge")
