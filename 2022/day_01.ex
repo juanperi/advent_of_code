8cbd6df43d9fd6f82a78fcef88dc3dfcb019dcd9
Juan Peri <eternoperegrino@gmail.com>
2022-12-01 11:47:29 +0100

    refactored into support module

--- 2022/day_01.ex
+++ 2022/day_01.ex
@@ -1,14 +1,15 @@
 defmodule Day01 do
   def run() do
+    input = Support.input()
     result =
-      input()
+      input
       |> Enum.map(&Enum.sum/1)
       |> Enum.max()
 
     IO.inspect(result, label: "Part 1")
 
     result =
-      input()
+      input
       |> Enum.map(&Enum.sum/1)
       |> Enum.sort(:desc)
       |> Enum.take(3)
@@ -16,8 +17,10 @@ defmodule Day01 do
 
     IO.inspect(result, label: "Part 2")
   end
+end
 
-  defp input() do
+defmodule Support do
+  def input() do
     File.read!("day_01.txt")
     |> String.split("\n\n", trim: true)
     |> Enum.map(fn row ->
