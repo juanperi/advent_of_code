b80c6ae9b767aa88d122abef88df82d2aaecd540
Juan Peri <eternoperegrino@gmail.com>
2022-12-01 08:04:58 +0100

    2022-01
    2022-01

---
+++ 2022/day_01.ex
@@ -0,0 +1,31 @@
+defmodule Day01 do
+  def run() do
+    result =
+      input()
+      |> Enum.map(&Enum.sum/1)
+      |> Enum.max()
+
+    IO.inspect(result, label: "Part 1")
+
+    result =
+      input()
+      |> Enum.map(&Enum.sum/1)
+      |> Enum.sort(:desc)
+      |> Enum.take(3)
+      |> Enum.sum()
+
+    IO.inspect(result, label: "Part 2")
+  end
+
+  defp input() do
+    File.read!("day_01.txt")
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn row ->
+      row
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
+  end
+end
+
+Day01.run()
